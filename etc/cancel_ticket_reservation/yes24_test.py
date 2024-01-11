d2820c43311a4d63e916ee7a578285bebc66330b
dongsikchoi <dongsikchoi.dev@gmail.com>
2024-01-11 21:45:17 +0900

    [MOD] 그래24 완성!

--- etc/cancel_ticket_reservation/yes24_test.py
+++ etc/cancel_ticket_reservation/yes24_test.py
@@ -30,6 +30,8 @@ chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 chrome_options.add_argument('--disable-popup-blocking')
 chrome_options.add_argument('--ignore-certificate-errors') #Error parsing cert retrieved from AIA (as DER): << 에러 방지
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
+
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 driver.get(config.TARGET_SITE_YES24)
@@ -55,3 +57,49 @@ time.sleep(3)
 date_select = driver.find_element(By.XPATH,"/html/body/div[6]/div[1]/div[2]/div[1]/div[1]/div[2]/table/tbody/tr[4]/td[7]/a").click()
 time.sleep(1)
 next_step = driver.find_element(By.XPATH,"/html/body/div[6]/div[1]/div[2]/div[1]/div[2]/div[2]/div/div[4]/div/img").click()
+wait = WebDriverWait(driver, 5)  
+time.sleep(5)
+
+iframe_element = driver.find_element(By.TAG_NAME,'iframe')
+driver.switch_to.frame(iframe_element)
+inner_html_element = driver.find_element(By.TAG_NAME,'html')
+inner_html_text =inner_html_element.text
+count = 0
+is_success=False
+while True:
+    count +=1
+    if count % 2 == 0:
+        driver.find_element(By.ID,'area1').click()
+        time.sleep(1)
+        div_tags = driver.find_elements(By.XPATH,"//div[@id='divSeatArray']/div")
+
+        for div_tag in div_tags:
+            value = div_tag.get_attribute("class")
+            if value != 's13':
+                div_tag.click()
+                is_success=True
+                break
+
+        if is_success:
+            break
+
+    else:
+        driver.find_element(By.ID,'area0').click()
+        time.sleep(1)
+        div_tags = driver.find_elements(By.XPATH,"//div[@id='divSeatArray']/div")
+        for div_tag in div_tags:
+            value = div_tag.get_attribute("class")
+            if value != 's13':
+                div_tag.click()
+                is_success=True
+                break
+        if is_success:
+            break
+    if count % 10 == 0:
+        print(f'{count} 회..')
+    time.sleep(random.uniform(1,3) )
+if is_success:
+    driver.execute_script("alert('알람 테스트');")
+
+
+print('end')
